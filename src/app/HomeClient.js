c35244c341141047229b9aad97d05851a1d9b8cf
nischay <nischayreddy.t@gmail.com>
2026-05-05 21:11:22 +0530

    customization

--- src/app/HomeClient.js
+++ src/app/HomeClient.js
@@ -75,7 +75,7 @@ export default function HomeClient() {
       {/* WELCOME POPUP MODAL */}
       {showPopup && (
         <div className="fixed inset-0 z-[100] flex items-center justify-center p-4 bg-black/80 backdrop-blur-sm animate-fade-in">
-          <div className="bg-[#080d1a] border border-slate-700 rounded-2xl w-full max-w-md shadow-2xl overflow-hidden relative">
+          <div className="bg-[#f3f6fc] border border-slate-700 rounded-2xl w-full max-w-md shadow-2xl overflow-hidden relative">
             
             {/* Close Button matching reference image style */}
             <button onClick={closePopup} className="absolute top-4 right-4 bg-white rounded-full p-1.5 text-[#111827] hover:bg-slate-200 transition z-10 shadow-md">
